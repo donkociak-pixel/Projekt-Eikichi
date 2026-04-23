0a46d43fe2bd3dbffd524e5f0a5d1a1fee2e6e64
Aineko <donkociak@gmail.com>
2026-04-23 21:31:45 +0200

    zmiana z langchain na ollam żeby przyśpeiszyć działanie modelu oraz dodanie obsługi persony testowej

--- core/brain.py
+++ core/brain.py
@@ -1,11 +1,19 @@
+import json
+import ollama
 
-
-from langchain_ollama import OllamaLLM
-
-
-
-lllm = OllamaLLM(model="qwen3.5:9b", extra_body={"enable_thinking": False})
 def zapytanie(user_prompt):
-    prompt = lllm.invoke(user_prompt)
-    print(prompt)
-    return prompt
+    with open("persony/nauczyciele/matematyka.json", "r") as f:
+        dane = json.load(f)
+    
+    odpowiedz = ollama.chat(
+        model="qwen3.5:9b",
+        messages=[
+            {"role": "system", "content": dane["prompt"]},
+            {"role": "user", "content": user_prompt}
+        ],
+        options={"num_predict": 512},
+        think=False
+    )
+    wynik = odpowiedz["message"]["content"]
+    print(wynik)
+    return wynik
