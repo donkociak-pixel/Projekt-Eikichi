1e75ea8a6e2295ed3a35b4d4d0b12724710c4eed
Aineko <donkociak@gmail.com>
2026-04-27 19:04:54 +0200

    zmiana zasad działania konwertera

--- core/converter.py
+++ core/converter.py
@@ -2,11 +2,14 @@ import pdfplumber
 import os
 
 
-def konwersja(sciezka_pliku):
-    plik_wiedza=pdfplumber.open(sciezka_pliku)
-    text=""
-    for strona in plik_wiedza:
-        zawartosc = strona.extract_text()
-        if zawartosc == None:
-            continue
-        text = text + zawartosc
+def ekstraktor(sciezka_pliku):
+    with pdfplumber.open(sciezka_pliku) as plik:
+        liczba_stron = len(plik.pages)
+        dane_wyjsciowe = []
+        for numer_strony ,strona in enumerate(plik.pages):
+            zawartosc = strona.extract_text()
+            if zawartosc == None:
+                continue
+            slownik = {"tekst": zawartosc, "metadata": {"source": sciezka_pliku, "page": numer_strony}}
+            dane_wyjsciowe.append(slownik)
+    return dane_wyjsciowe
